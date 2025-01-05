2bb7b63b2d3b7913a532cc2026b6dcfa0661b0cc
AndMTech <andmtech@gmail.com>
2025-01-05 22:10:44 +0530

    Add files via upload

---
+++ src/com/api/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.api;
+
+/**
+ * @Author John 05-01-2025
+ */
+public class HelloWorld {
+
+	public static void main(String args[]) {
+
+		// TODO need to add more print statements
+		System.out.println("Hello World to meeee!!");
+
+		/*
+		 * This constant is to print PI value
+		 */
+		final double PI = 3.14;
+		System.out.println("This is my constant PI value: " + PI);
+
+		int age = 25;
+		System.out.println("This is my variable age value: " + age);
+
+	}
+
+}
